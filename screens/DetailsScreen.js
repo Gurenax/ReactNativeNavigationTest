c25b84a5acd9bd99c5455fdf7d4ec86e92d7f6db
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-21 16:52:43 +1000

    Add push, navigate and goBack buttons

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react'
-import {StyleSheet, Text, View} from 'react-native'
+import {StyleSheet, Text, View, Button} from 'react-native'
 
 class DetailsScreen extends Component {
   render() {
+    const push = this.props.navigation.push
+    const navigate = this.props.navigation.navigate
+    const goBack = this.props.navigation.goBack
     return (
       <View style={styles.container}>
         <Text>Details Screen</Text>
+        <Button
+          title="Go to Details..again"
+          onPress={() => push('Details')}/>
+        <Button
+          title="Go to Home"
+          onPress={() => navigate('Home')}/>
+        <Button
+          title="Go back"
+          onPress={() => goBack()}/>
       </View>
     )
   }
