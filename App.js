76b3daf8c269bacaa950c8fc8739603a06f09b77
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-08-22 11:20:20 +1000

    Add HeaderWithButtons component

--- App.js
+++ App.js
@@ -10,11 +10,13 @@ import { createStackNavigator } from 'react-navigation'
 // Other Screens
 import HomeScreen from './screens/HomeScreen'
 import DetailsScreen from './screens/DetailsScreen'
+import HeaderWithButtonsScreen from './screens/HeaderWithButtonsScreen'
 
 const RootStack = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen
+    Details: DetailsScreen,
+    HeaderWithButtons: HeaderWithButtonsScreen
   },
   {
     initialRouteName: 'Home'
